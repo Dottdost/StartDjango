239189e8f55d650d74cffa79baa7cdb543034662
Самед-заде Гамида Эльман <samed_gh90@itstep.edu.az>
2025-09-29 21:52:37 +0400

    new

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from . import views
+
+app_name="todo"
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:task_id>', views.detail, name='detail'),
+    path('index/', views.index, name='index'),
+
+
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from gc import get_objects
+
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from todo import models
@@ -7,8 +9,15 @@ from todo import models
 # Create your views here.
 def index(request):
     tasks = models.Task.objects.all()
-    return HttpResponse([str(tasks)+'<br/>' for tasks in tasks])
+    # return HttpResponse([str(tasks) + '<br/>' for tasks in tasks])
     # return HttpResponse("todo")
-    return render(request, 'task.html', {'tasks':tasks})
+    return render(request, 'tasks.html', {'tasks': tasks})
 
 
+def detail(request, task_id):
+    task = models.Task.objects.get(pk=task_id)
+    return render(request, 'detail.html', {'task': task})
+
+
+    task = get_object_or_404(models.Task, pk=task_id)
+    return render(request, 'detail.html', {'task': task})
